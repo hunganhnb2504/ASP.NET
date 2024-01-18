642763230504bc3d0a14363b1cb881eb260186f3
hunganhnb2504 <hunganhnbng@gmail.com>
2024-01-18 08:26:44 +0700

    moi

---
+++ WebApplication4/WebApplication4/Data/WebApplication4Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication4.Models;
+
+namespace WebApplication4.Data
+{
+    public class WebApplication4Context : DbContext
+    {
+        public WebApplication4Context (DbContextOptions<WebApplication4Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication4.Models.Student> Student { get; set; } = default!;
+    }
+}
---
+++ WebApplication4/WebApplication4/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication4.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+
+        public string? Phone { get; set; }
+    }
+}
