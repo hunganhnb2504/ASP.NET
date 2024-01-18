4fb7f1a6db5063c868a273ad7753b481ceec7304
hunganhnb2504 <hunganhnbng@gmail.com>
2024-01-18 09:08:53 +0700

    moi

--- WebApplication4/WebApplication4/Models/Student.cs
+++ WebApplication4/WebApplication4/Models/Student.cs
@@ -1,11 +1,15 @@
-﻿namespace WebApplication4.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication4.Models
 {
     public class Student
     {
         public int Id { get; set; }
+        [Required]
         public string? Name { get; set; }
+        [EmailAddress]
         public string? Email { get; set; }
-
+        [Phone]
         public string? Phone { get; set; }
     }
 }
